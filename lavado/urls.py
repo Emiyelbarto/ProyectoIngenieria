7cf5459dd73cc09130f79ed7352051c426c27fb9
Emiliano Perez <emiliano@anunciatedigital.com>
2020-03-04 19:48:48 -0600

    fixes

--- lavado/urls.py
+++ lavado/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('paquetes.html', views.paquetes, name="paquetes"),
+    path('paquetes.html', views.paquetes, name="paquetes"),
     path('index', views.index, name="index"),
     path('paquetes', views.paquetes, name="paquetes"),
     path('dashboard', views.dashboard, name="dashboard"),
